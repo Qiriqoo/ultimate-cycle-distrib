d9fa3808f07aaf78026619c00d469a3194aab9e5
Alexandre Ktifa <alexandre.ktifa@gmail.com>
2015-02-18 13:42:41 +0100

    Let admin to activate user

--- app/overrides/add_activate_to_users_form.rb
+++ app/overrides/add_activate_to_users_form.rb
@@ -1,7 +1,7 @@
 Deface::Override.new(:virtual_path => "spree/admin/users/index",
                      :name => "add_activate_button_to_users_index",
                      :insert_bottom => "td.user_email",
-                     :text => "<span class='pull-right'><%= user.is_admin? ? Spree.t(:admin) : Spree.t(:enterprise) %> - <%= user.active ? Spree.t(:active) : Spree.t(:inactive) %></span>")
+                     :text => "<span class='pull-right'><%= user.is_admin? ? Spree.t(:administrator) : Spree.t(:enterprise) %> - <%= user.active ? Spree.t(:active) : Spree.t(:inactive) %></span>")
 
 Deface::Override.new(:virtual_path => "spree/admin/users/_form",
                      :name => "add_activate_button_to_users_index2",
