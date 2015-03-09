88e234047708db8300bec9bc5b8c6fa83ee70ebc
Alexandre Ktifa <alexandre.ktifa@gmail.com>
2015-03-09 16:02:39 +0100

    Add some export controller spec

--- spec/controllers/exports_controller_spec.rb
+++ spec/controllers/exports_controller_spec.rb
@@ -2,6 +2,37 @@ require 'rails_helper'
 
 describe Spree::Admin::ExportsController do
 
+  describe '#index' do
+
+    let(:export_newsletter) do
+      Spree::Export.create!(source: :newsletters, status: :available)
+    end
+    let(:export_order) do
+      Spree::Export.create!(source: :orders)
+    end
+
+    before(:each) do
+      @routes = Spree::Core::Engine.routes
+      controller.stub :spree_current_user => FactoryGirl.create(:admin_user)
+    end
+
+    subject { get :index }
+
+    it 'assigns the lastest exports date' do
+      export_order
+      export_newsletter
+      subject
+
+      expect(assigns(:last_exports)).to match_array(:newsletters => Spree::Export.newsletters.maximum(:created_at), :orders => nil)
+    end
+
+    it 'assigns exports in progress' do
+      export_order
+      subject
+      expect(assigns(:exports_in_progress)).to match_array(:newsletters => false, :orders => true)
+    end
+  end
+
   describe '#create' do
 
     before(:each) do
