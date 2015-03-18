6f8f59a92021bcd57d30a14866e738aa5b7f9e44
Alexandre Ktifa <alexandre.ktifa@gmail.com>
2015-03-18 17:42:30 +0100

    Refactor admin config view

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,6 +15,14 @@
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
+
+  config.before(:each) do
+    FactoryGirl.create(:store)
+  end
+
+  config.after(:each) do
+    Spree::Store.destroy_all
+  end
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
