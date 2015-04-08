b2c5b2629d24a2d8bec897164d6e48622b440f9d
Alexandre Ktifa <alexandre.ktifa@gmail.com>
2015-04-08 13:04:45 +0200

    Re setup prod config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,8 +20,8 @@ Rails.application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  # config.serve_static_files = true
-  # config.static_cache_control = 'public, max-age=31536000'
+  config.serve_static_files = true
+  config.static_cache_control = 'public, max-age=31536000'
 
 
   # Compress JavaScripts and CSS.
@@ -53,7 +53,15 @@ Rails.application.configure do
   # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
 
   # Use a different cache store in production.
-
+  config.cache_store = :dalli_store,
+                    (ENV["MEMCACHIER_SERVERS"] || "").split(","),
+                    {username: ENV["MEMCACHIER_USERNAME"],
+                     password: ENV["MEMCACHIER_PASSWORD"],
+                     failover: true,
+                     compress: true,
+                     socket_timeout: 1.5,
+                     socket_failure_delay: 0.2
+                    }
 
   config.paperclip_defaults = {
     :storage => :s3,
