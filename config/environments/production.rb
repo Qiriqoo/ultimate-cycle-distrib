d1358ff58e6e6804fb6dddf7dc23b4d368f8c7a8
Alexandre Ktifa <alexandre.ktifa@gmail.com>
2015-04-08 12:05:28 +0200

    Prod cache / s3 setup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -53,7 +53,6 @@ Rails.application.configure do
   # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
 
   # Use a different cache store in production.
-  config.cache_store = :dalli_store, nil, { namespace: 'UltimateCycle', compress: true }
   config.cache_store = :dalli_store,
                     (ENV["MEMCACHIER_SERVERS"] || "").split(","),
                     {username: ENV["MEMCACHIER_USERNAME"],
@@ -63,6 +62,13 @@ Rails.application.configure do
                      socket_timeout: 1.5,
                      socket_failure_delay: 0.2
                     }
+
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => 'ucd-s3-prod'
+    }
+  }
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = "http://assets.example.com"
