20f53f7c683c4f242fdce3c4a3d245be76b125a9
Alexandre Ktifa <alexandre.ktifa@gmail.com>
2015-04-10 13:57:19 +0200

    Fix cache and images

--- app/models/spree/product_extension.rb
+++ app/models/spree/product_extension.rb
@@ -8,16 +8,13 @@ module ProductExtension
   module InstanceMethods
 
     def get_cross_products
-      return Spree::Product.order('RANDOM()').limit(3) if taxons.blank?
+      return Spree::Product.order('RANDOM()').limit(3).distinct if taxons.blank?
 
       Spree::Product.joins(:taxons)
                     .where('spree_taxons.id IN (?)', taxons.pluck(:id))
                     .where('spree_products.id != ?', id)
                     .limit(3)
-    end
-
-    def image_path(size = 'mini')
-      images.first.attachment.url(size.to_sym)
+                    .distinct
     end
   end
 end
